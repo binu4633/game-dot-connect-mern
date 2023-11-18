3e5bb34c06acd9e8a55088e2aab778f9897ab6f6
binu4633 <binu4633@gmail.com>
2023-11-19 02:44:35 +0530

    ninth commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,13 +6,15 @@ const connectDB = async()=>{
 
      let uri ;
      
-     if(process.env.NODE_ENV = 'development'){
-        uri = await process.env.MONGO_URI
-     }else{
+     if(process.env.NODE_ENV === 'production'){
+        console.log('does development workkssss')
         uri = await process.env.MONGO_URI2
+     }else{
+        uri = await process.env.MONGO_URI
      }
-      
-      const conn = await mongoose.connect(process.env.MONGO_URI) 
+      console.log('uri', uri)
+    //   const conn = await mongoose.connect(process.env.MONGO_URI) 
+      const conn = await mongoose.connect(uri) 
       
      console.log(`Mongodb connected: ${conn.connection.host}`);
     } catch (error) {
